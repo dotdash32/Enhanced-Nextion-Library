821c9cc606b8cee2d0fcbe18116d5251d111a364
Josh DeWitt <dotdash32@gmail.com>
2022-08-30 15:39:50 -0700

    chore: change array sizes for name uniqueness
    should make it easier to use PIO build_flags to configure at runtime, because
    we also enabled a series of #ifndefs

--- include/NexConfig.h
+++ include/NexConfig.h
@@ -1,19 +1,19 @@
 /**
  * @file NexConfig.h
  *
- * Options for user can be found here. 
+ * Options for user can be found here.
  *
  * @author  Wu Pengfei (email:<pengfei.wu@itead.cc>)
  * @date    2015/8/13
  * @author Jyrki Berg 2/17/2019 (https://github.com/jyberg)
- * 
- * @copyright 
+ *
+ * @copyright
  * Copyright (C) 2014-2015 ITEAD Intelligent Systems Co., Ltd. \n
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
- * 
+ *
  * @copyright 2020 Jyrki Berg
  *
  */
@@ -21,21 +21,21 @@
 #pragma once
 
 /**
- * @addtogroup Configuration 
- * @{ 
+ * @addtogroup Configuration
+ * @{
  */
 
 /**
  * Define serial communication default baud.
  * it is recommended that do not change defaul baud on Nextion, because it can forgot it on re-start
  * If changing this value check that vakue is the same as factory set default baud in the used display.
- * 
+ *
 */
 #define NEX_SERIAL_DEFAULT_BAUD 9600
 
 /**
  * Define standard (dafault) or fast timeout,  you may use fast timeout in case of baudrate higher than 115200
- * 
+ *
 */
 #define NEX_TIMEOUT_STANDARD
 //#define NEX_TIMEOUT_FAST
@@ -55,34 +55,43 @@
 
 /**
  * Serial Recieving Buffer Sizes
- * 
+ *
  * RX_BUFFER_SIZE is the primary buffer that is read into directly after
  * SerialX.read.  It needs to be at least 72 (for comok), but should be larger.
- * 
+ *
  * CMD_QUEUE_SIZE is the depth of sent Event Queue.  It keeps track of sent events
  * and how to respond in code
- * 
+ *
  * RX_2ND_BUF_SIZE is the size of the secondary buffer (in an array).  It is used
  * to track responses to respond during blocking loops
- * 
+ *
  * RX_2ND_ARR_SIZE is the number of entries in the FIFO-like array to store
  * returned messages. (soft limited to 127 spots)
  */
-#define RX_BUFFER_SIZE      128 // primary data buffer length
-#define CMD_QUEUE_SIZE      8 // depth of sent event queue
-#define RX_2ND_BUF_SIZE     RX_BUFFER_SIZE // width of secondary buffer for responses
-#define RX_2ND_ARR_SIZE     8   // depth of secondary buffer
+#ifndef NEX_RX_BUFFER_SIZE
+  #define NEX_RX_BUFFER_SIZE      128 // primary data buffer length
+#endif
+
+#ifndef NEX_CMD_QUEUE_SIZE
+  #define NEX_CMD_QUEUE_SIZE      8 // depth of sent event queue
+#endif
+#ifndef NEX_RESP_BUF_SIZE
+  #define NEX_RESP_BUF_SIZE       NEX_RX_BUFFER_SIZE // width of secondary buffer for responses
+#endif
+#ifndef NEX_RESP_ARR_SIZE
+  #define NEX_RESP_ARR_SIZE       8   // depth of secondary buffer
+#endif
 
 
 
-/** 
- * Define DEBUG_SERIAL_ENABLE to enable debug serial. 
- * Comment it to disable debug serial. 
+/**
+ * Define DEBUG_SERIAL_ENABLE to enable debug serial.
+ * Comment it to disable debug serial.
  */
 //#define DEBUG_SERIAL_ENABLE
 
 /**
- * Define dbSerial for the output of debug messages. 
+ * Define dbSerial for the output of debug messages.
  * it is resonsibility of main program to initialize debug serial port (begin(...)
  */
 //#define dbSerial Serial
